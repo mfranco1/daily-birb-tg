749e5ebcfd97d5bc9f91e8652e39544758cf3948
Marcelino Franco <marcelino.franco@swapoolabs.com>
2021-06-29 16:54:15 +0800

    Add sending to dynamic tg chat group

--- daily_birb.go
+++ daily_birb.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "log"
     "html"
     "math/rand"
@@ -12,18 +13,27 @@ import (
 )
 
 func main() {
+    message := createMessage()
+    log.Printf("%s", message)
+    chat_id, err := strconv.ParseInt(os.Getenv("TG_CHAT_ID"), 10, 64)
+    chat_object := tgbotapi.NewMessage(chat_id, message)
+
     bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_TOKEN"))
     if err != nil {
         log.Panic(err)
     }
-
     bot.Debug = true
     log.Printf("Authorized on account %s", bot.Self.UserName)
-    log.Printf("%s", randomBirb())
+
+    bot.Send(chat_object)
 }
 
 func init() {
     rand.Seed(time.Now().UnixNano())
+}
+
+func createMessage() string {
+    return fmt.Sprintf("Time for your daily birb %s", randomBirb())
 }
 
 func randomBirb() string {
